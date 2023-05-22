22eb9df6cfb727b3300d94d72c35aaca0a7b9d39
guilherme <guguiejo@gmail.com>
2023-05-22 11:54:09 -0300

    mudando background com horarios

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 function carregar(){
 var msg = window.document.getElementById('msg')
 var img = window.document.getElementById('imag')
-var data = new Date()
-//var hora = data.getHours()
-var hora = 11
+//var data = new Date()
+var hora = data.getHours()
+var hora = 14
 msg.innerHTML = ` Agora São ${hora} horas. `
 
 if (hora > 0 && hora < 12) {
     //BOM DIA 
-    document.body.style.background = '#f3cb65';
     img.src = 'imagens/dia.jpg'
+    document.body.style.background = 'rgb(133, 218, 242)'
 } else if (hora >= 12 && hora < 18) {
-    //BOA TARDE
-    imag.style.width = '200px';
-    imag.style.height = '200px';
-    img.src= 'imagens/tarde.jpg'
+    //BOA TARDE 
+    img.src= 'imagens/otatarde.jpg'
+    document.body.style.background = "#FEB30D"
+    img.style.height = '300px';
+    img.style.marginLeft = '-120px';
+
 } else {
     //BOA NOITE
     img.src= 'imagens/noite.jpg'
+    document.body.style.background = '#756B85'
 }
 
 
